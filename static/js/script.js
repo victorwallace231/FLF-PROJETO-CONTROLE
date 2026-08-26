31999bafa10a3aed1ac96ef422af80f9845e2868
derik.santos@aluno.ce.gov.br <derik.santos@aluno.ce.gov.br>
2026-08-26 15:01:24 -0300

    HTML E JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,34 @@
-const usuarioLogado = localStorage.getItem("usuarioLogado");
-
-if (!usuarioLogado) {
-    window.location.href = "login.html";
-}
-
 function efetuarLogin() {
+    /*AQUI ABAIXO BANCO DE DADOS*/
     localStorage.setItem("usuarioLogado", "true");
   
     window.location.href = "index.html";
 }
 
 function fazerLogout() {
+
+    /*BANCO DE DADOS NA LINHA ABAIXO*/
     localStorage.removeItem("usuarioLogado");
     window.location.href = "login.html";
 }
+
+document.getElementById('form-cad').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nome = document.getElementbyId('cadNome').value;
+    const email = document.getElementById('cadEmail').value;
+    const senha = document.getElementById('cadSenha').value;
+    const telefone = document.getElementById('cadTelefone');
+
+
+    /*TESTE TESTE TESTE BANCO DE DADOS É AQUI!!!!*/
+    localStorage.setItem('user_' + nome, email, senha, telefone);
+    alert('Conta criada com sucesso!');
+    this.reset();
+
+});
+
+document.getElementById('form-login').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('')
+})
