5f47011f3b4ca4e0a6f9dd3bd065bd8ef50fe7be
victorwallace231 <victorwallace102030@gmail.com>
2026-08-27 13:33:15 -0300

    Adicionando sessão e comentando o codigo

--- banco.py
+++ banco.py
@@ -1,4 +1,6 @@
 import sqlite3
+
+# Função para conectar ao banco de dados
 def conectar_banco():
     conexao= sqlite3.connect("Controle.db")
     return conexao
--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request ,session
 from banco import conectar_banco
 
+# Blueprint para a rota do dashboard
 route_dashboard = Blueprint('dashboard', __name__)
+
+# Rota do dashboard
 @route_dashboard.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+    # Conecta ao banco de dados
     conexao = conectar_banco()
     cursor = conexao.cursor()
 
-    cursor.execute("SELECT * FROM usuario")
-    usuario = cursor.fetchall()
-    conexao.close()
-
-    return render_template('dashboard.html', usuarios=usuario)
+    name = session.get('usuario_name')  # Obtém o nome do usuário da sessão
+    email = session.get('usuario_email')  # Obtém o email do usuário da sessão
+    telefone = session.get('usuario_telefone')  # Obtém o telefone do usuário da sessão
+    # Renderiza o template do dashboard com os dados do usuário
+    return render_template('dashboard.html', name=name, email=email, telefone=telefone)
