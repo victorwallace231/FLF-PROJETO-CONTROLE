34164ea9424ae9f21e163b8c7b409b0b6f35422d
victorwallace231 <victorwallace102030@gmail.com>
2026-09-03 16:07:36 -0300

    logica dos balões do dashboard

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -3,6 +3,7 @@ from banco import conectar_banco
 
 # Blueprint para a rota do dashboard
 route_dashboard = Blueprint('dashboard', __name__)
+route_equipamentos = Blueprint('equipamentos', __name__)
 
 # Rota do dashboard
 @route_dashboard.route('/dashboard', methods=['GET', 'POST'])
@@ -20,5 +21,31 @@ def dashboard():
 
     cursor.execute("SELECT * FROM perifericos")
     perifericos = cursor.fetchall()
+    total_perifericos = len(perifericos)
+    disponiveis = 0
+    usados = 0
+    manutencao = 0
+    for periferico in perifericos:
+        if periferico[4] == True:
+            disponiveis += 1
+        elif periferico[4] == False:
+            usados += 1
+        elif periferico[4] == None and periferico[5] == True:
+            manutencao += 1
+
     conexao.close()
-    return render_template('dashboard.html', name=name, email=email, telefone=telefone, perifericos=perifericos)
+    return render_template('dashboard.html', name=name, email=email, telefone=telefone, perifericos=perifericos, disponiveis=disponiveis, usados=usados, manutencao=manutencao, total_perifericos=total_perifericos)
+
+@route_equipamentos.route('/equipamentos', methods=['GET', 'POST'])
+def equipamentos():
+    if not session.get("usuario_email"):
+        return redirect('/login')
+    if request.method == 'GET':
+        return render_template('equipamentos.html', name=session.get('usuario_name'))
+    if request.method == 'POST':
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM perifericos")
+        perifericos = cursor.fetchall()
+        conexao.close()
+        return render_template('equipamentos.html', name=session.get('usuario_name'), perifericos=perifericos)
