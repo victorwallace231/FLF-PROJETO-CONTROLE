cf9b83fca0d34ecfcf2c2da5aae7040f5e182572
victorwallace231 <victorwallace102030@gmail.com>
2026-09-16 14:41:33 -0300

    Projeto Controle 1.0

--- routes/user_historico.py
+++ routes/user_historico.py
@@ -3,12 +3,13 @@ from banco import conectar_banco
 
 user_historico = Blueprint('user_historico', __name__)
 
-@user_historico.route('/user_historico', methods = ['POST'])
-def actvation():
-    if request.method == 'POST':
+@user_historico.route('/user_historico', methods = ['GET','POST'])
+def historico():
+    if request.method == 'GET':
         conexao = conectar_banco()
         cursor = conexao.cursor()
-        cursor.execute("SELECT * FROM emprestimos WHERE id_usuario = ?", (session.get("usuario_id")))
+        id_usuario = session.get("usuario_id")
+        cursor.execute("SELECT * FROM emprestimos WHERE id_usuario = ?", (id_usuario,))
         emprestimos = cursor.fetchall()
         conexao.close()
         return render_template("historico.html", emprestimos = emprestimos)
