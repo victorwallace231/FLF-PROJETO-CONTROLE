73a8745780853b435b337c7ba13990b45b29dbc7
joaquim234156 <joaquimbismarck23@gmail.com>
2026-09-22 14:41:12 -0300

    estilização dos botões no createitem

--- static/js/funcionalidadeCategoria.js
+++ static/js/funcionalidadeCategoria.js
@@ -3,16 +3,16 @@ async function adicionarCategoria() {
 
     container.innerHTML = `
     
-    <h3>Adicione uam Nova Categoria</h3>
+    <h3>Adicione uma Nova Categoria</h3>
     <form action="/create_categoria" method="POST">
-    <div class="input-group mb-3">
-        <span class="input-group-text border-icon"><i class="bi bi-hash"></i></span>
-        <input type="text" id="createInput" class="form-control" name="nome_categoria" placeholder="Digite a Nova Categoria" required>
-        <button type="submit" class=" w-100 text-dark text-black fw-semibold">
-
-        <a class=" btn btn-primary w-100 text-dark text-black text-block fw-semibold">Adicionar</a>
-        </button>
-    </div>
+        <div class="w-100">
+            <label for="createInput" class="form-label">Categoria</label>
+            <div class="input-group">
+                <span class="input-group-text border-icon"><i class="bi bi-hash"></i></span>
+                <input type="text" id="createInput" class="form-control" name="nome_categoria" placeholder="Digite a Nova Categoria" required>
+                <button type="submit" class="btn btn-primary w-100 text-black fw-semibold d-flex justify-content-center align-content-center py-2 mt-3 rounded-pill">Adicionar</button>
+            </div>
+        </div>
     </form>
 
     `
@@ -23,7 +23,7 @@ async function deletarCategoria(categorias){
     const cats = categorias
     container.innerHTML = `
     
-    <h3>Adicione uam Nova Categoria</h3>
+    <h3>Adicione uma Nova Categoria</h3>
     <form action="/delete_categoria" method="POST">
     <div class="input-group mb-3">
         <span class="input-group-text border-icon"><i class="bi bi-hash"></i></span>
@@ -58,7 +58,7 @@ async function modificarCategoria(){
 
     container.innerHTML = `
     
-    <h3>Adicione uam Nova Categoria</h3>
+    <h3>Adicione uma Nova Categoria</h3>
     <form action="" method="POST">
     <div class="input-group mb-3">
         <span class="input-group-text border-icon"><i class="bi bi-hash"></i></span>
